f7542654e12258eb378ad52bd589a7edceee4b66
Rob Righter <rrighter@rrighter-ltm.internal.salesforce.com>
2018-04-27 12:07:37 -0400

    CSV writer stuff

---
+++ src/main/kotlin/com/djvk/sarPdfParser/CsvHeaders.kt
@@ -0,0 +1,11 @@
+package com.djvk.sarPdfParser
+
+import com.sun.xml.internal.fastinfoset.util.StringArray
+
+class CsvHeaders {
+    val DOC_SAR = arrayOf(
+            "Student Last Name",
+            "Student First Name"
+    //todo add more
+    )
+}
---
+++ src/main/kotlin/com/djvk/sarPdfParser/CsvWriter.kt
@@ -0,0 +1,27 @@
+package com.djvk.sarPdfParser
+
+import java.util.*
+
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVPrinter
+
+import java.io.*
+import java.nio.file.Files
+import java.nio.file.Paths
+
+public class CsvWriter(outFile: String) {
+
+    fun init() {
+
+    }
+
+    fun insertRow(row: Array<Map<String,String>>){
+
+    }
+
+    fun finish(){
+
+    }
+
+
+}
