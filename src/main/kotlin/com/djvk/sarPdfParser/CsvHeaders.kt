38db53832d10799ea4d5582367de3155506fe99a
Rob Righter <rrighter@rrighter-ltm.internal.salesforce.com>
2018-04-27 13:22:19 -0400

    CSV header definition file formatting

--- src/main/kotlin/com/djvk/sarPdfParser/CsvHeaders.kt
+++ src/main/kotlin/com/djvk/sarPdfParser/CsvHeaders.kt
@@ -3,9 +3,15 @@ package com.djvk.sarPdfParser
 import com.sun.xml.internal.fastinfoset.util.StringArray
 
 class CsvHeaders {
+
+    public var H_DSAR_STUDENT_FIRST_NAME = "Student Last Name"
+    public var H_DSAR_STUDENT_MIDDLE_NAME = "Student Middle Name"
+    public var H_DSAR_STUDENT_LAST_NAME = "Student Last Name"
+
     val DOC_SAR = arrayOf(
-            "Student Last Name",
-            "Student First Name"
+            H_DSAR_STUDENT_LAST_NAME,
+            H_DSAR_STUDENT_MIDDLE_NAME,
+            H_DSAR_STUDENT_LAST_NAME
     //todo add more
     )
 }
