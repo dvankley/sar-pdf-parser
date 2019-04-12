0b693206333b29d64e660e4143af1200e624644a
Ezra Chu <ezra.chu@salesforce.com>
2019-04-12 11:27:27 -0400

    branch pdfreader

--- src/main/kotlin/com/djvk/sarPdfParser/PdfReader.kt
+++ src/main/kotlin/com/djvk/sarPdfParser/PdfReader.kt
@@ -5,33 +5,63 @@ import kotlinx.coroutines.experimental.*
 import java.io.File
 
 class PdfReader(private val files: Array<File>) {
-    fun startProcessing() {
-        val parser = PdfParser()
-        val primaryCsvWriter = CsvWriter("outfile.csv", CsvHeaders.DocType.SAR)
-        val errorCsvWriter = CsvWriter("errors.csv", CsvHeaders.DocType.ERROR)
-        val context = CommonPool
-        val jerbs: MutableList<Pair<String, Deferred<Map<String, String>>>> = ArrayList()
-        files.forEach { file ->
-            jerbs.add(Pair(file.name, async(context, CoroutineStart.DEFAULT, {
-                parser.processFile(file)
-            })))
-        }
-        jerbs.forEach { (fileName, jerb) ->
-            runBlocking {
-                try {
-                    val output = jerb.await().toMutableMap()
-                    output[PdfNormalizer.normalizeField(CsvHeaders.Fields.FILENAME.csvFieldName)] = fileName
-                    primaryCsvWriter.insertRow(output)
-                } catch (e: Exception) {
-                    println("Error processing file $fileName ${e.dump()}")
-                    errorCsvWriter.insertRow(mapOf(
-                            PdfNormalizer.normalizeField(CsvHeaders.Fields.FILENAME.csvFieldName) to fileName,
-                            PdfNormalizer.normalizeField(CsvHeaders.Fields.ERROR.csvFieldName) to e.localizedMessage
-                    ))
+    fun startProcessing(fileType: String) {
+        if (fileType.equals("transcript")) {
+            val parser = TranscriptPdfParser()
+            val primaryCsvWriter = CsvWriter("outfile.csv", CsvHeaders.DocType.SAR)
+            val errorCsvWriter = CsvWriter("errors.csv", CsvHeaders.DocType.ERROR)
+            val context = CommonPool
+            val jerbs: MutableList<Pair<String, Deferred<Map<String, String>>>> = ArrayList()
+            files.forEach { file ->
+                jerbs.add(Pair(file.name, async(context, CoroutineStart.DEFAULT, {
+                    parser.processFile(file)
+                })))
+            }
+            jerbs.forEach { (fileName, jerb) ->
+                runBlocking {
+                    try {
+                        val output = jerb.await().toMutableMap()
+                        output[PdfNormalizer.normalizeField(CsvHeaders.Fields.FILENAME.csvFieldName)] = fileName
+                        primaryCsvWriter.insertRow(output)
+                    } catch (e: Exception) {
+                        println("Error processing file $fileName ${e.dump()}")
+                        errorCsvWriter.insertRow(mapOf(
+                                PdfNormalizer.normalizeField(CsvHeaders.Fields.FILENAME.csvFieldName) to fileName,
+                                PdfNormalizer.normalizeField(CsvHeaders.Fields.ERROR.csvFieldName) to e.localizedMessage
+                        ))
+                    }
                 }
             }
+            errorCsvWriter.finish()
+            primaryCsvWriter.finish()
+        } else {
+            val parser = PdfParser()
+            val primaryCsvWriter = CsvWriter("outfile.csv", CsvHeaders.DocType.SAR)
+            val errorCsvWriter = CsvWriter("errors.csv", CsvHeaders.DocType.ERROR)
+            val context = CommonPool
+            val jerbs: MutableList<Pair<String, Deferred<Map<String, String>>>> = ArrayList()
+            files.forEach { file ->
+                jerbs.add(Pair(file.name, async(context, CoroutineStart.DEFAULT, {
+                    parser.processFile(file)
+                })))
+            }
+            jerbs.forEach { (fileName, jerb) ->
+                runBlocking {
+                    try {
+                        val output = jerb.await().toMutableMap()
+                        output[PdfNormalizer.normalizeField(CsvHeaders.Fields.FILENAME.csvFieldName)] = fileName
+                        primaryCsvWriter.insertRow(output)
+                    } catch (e: Exception) {
+                        println("Error processing file $fileName ${e.dump()}")
+                        errorCsvWriter.insertRow(mapOf(
+                                PdfNormalizer.normalizeField(CsvHeaders.Fields.FILENAME.csvFieldName) to fileName,
+                                PdfNormalizer.normalizeField(CsvHeaders.Fields.ERROR.csvFieldName) to e.localizedMessage
+                        ))
+                    }
+                }
+            }
+            errorCsvWriter.finish()
+            primaryCsvWriter.finish()
         }
-        errorCsvWriter.finish()
-        primaryCsvWriter.finish()
     }
 }
